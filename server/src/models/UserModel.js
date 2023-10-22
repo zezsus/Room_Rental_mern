a7cc0e48cbfe94d96648171157616ebbcda8e26e
Sang <sang166464@huce.edu.vn>
2023-10-22 09:47:58 +0700

    user model

---
+++ server/src/models/UserModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("users", userSchema);
